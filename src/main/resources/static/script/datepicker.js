ec0dab1fdf4cc398e5d5e6bbd0c8172e72316379
PericlesKrH <perrykarach@gmail.com>
2020-04-20 19:38:56 +0300

    Merge origin/master into branch_Pericles
    Conflicts:
    	src/main/java/com/example/ContainerTerminal/controllers/UserController.java

--- src/main/resources/static/script/datepicker.js
+++ src/main/resources/static/script/datepicker.js
@@ -1,28 +1,34 @@
 $(document).ready(function () {
-                $("#datepicker").datepicker({format: 'dd-mm-yyyy'});
-                $("input[type=button]").datepicker({format: 'yyyy-mm-dd', autoclose: true, startDate: new Date()});
-            });
-            function jsPrintAll() {
-                setTimeout(function () {
-                    window.print();
-                }, 500);
-            }
-            function parseDate(input) {
-                // Transform date from text to date
-                var parts = input.match(/(\d+)/g);
-                // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
-                return new Date(parts[0], parts[1] - 1, parts[2]); // months are 0-based
-            }
-            function allagi(index) {
-                var start = parseDate($("#datepicker" + index).val());
-                console.log(start);
-                var end = parseDate($("#datepicker1" + index).val());
-                console.log(end);
-                var days = (Math.ceil((end - start) / (1000 * 60 * 60 * 24))) - 1;
-                console.log(days);
-                if (days <= 0) {
-                    $("#showDiff" + index).val(0 + " * 40$ = " + (0 * 40));
-                } else {
-                    $("#showDiff" + index).val(days + " * 40$ = " + (days * 40));
-                }
-            }
+    $("#datepicker").datepicker({format: 'dd-mm-yyyy'});
+    $("input[type=button]").datepicker({format: 'yyyy-mm-dd', autoclose: true, startDate: new Date()});
+});
+function jsPrintAll() {
+    setTimeout(function () {
+        window.print();
+    }, 500);
+}
+function parseDate(input) {
+    // Transform date from text to date    
+    var parts = input.match(/(\d+)/g);
+    // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
+    return new Date(parts[0], parts[1] - 1, parts[2]); // months are 0-based
+
+//    var parts = input.split("-");
+//    var date = new Date(parts[2], parts[1] - 1, parts[0]);
+//     return date;
+}
+function allagi(index) {
+    console.log($("#datepicker" + index).val());
+    var start = parseDate($("#datepicker" + index).val());
+    console.log(start);
+    console.log($("#datepicker1" + index).val());
+    var end = parseDate($("#datepicker1" + index).val());
+    console.log(end);
+    var days = (Math.ceil((end - start) / (1000 * 60 * 60 * 24))) - 1;
+    console.log(days);
+    if (days <= 0) {
+        $("#showDiff" + index).val(0 + " * 40$ = " + (0 * 40));
+    } else {
+        $("#showDiff" + index).val(days + " * 40$ = " + (days * 40));
+    }
+}
